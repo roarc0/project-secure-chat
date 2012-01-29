dd6aab780496b457043198edd189d037f7f8e6da
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2012-01-29 13:05:59 +0100

    * [WIP] aggiornato sistema di configurazione in modo da utilizzare lo stesso codice   da client e da server comodamente.

--- src/client/config-client.cpp
+++ src/client/config-client.cpp
@@ -1,84 +1,48 @@
 #include "config-client.h"
 
-cfg_opt_t opts[] =
-{   
-    CFG_BOOL((char*)"debug", (cfg_bool_t)false, CFGF_NONE),
-    CFG_STR((char*)"conf_filename",(char*)"pschat-client.conf",CFGF_NONE),
-    CFG_BOOL((char*)"log_enable",(cfg_bool_t)true, CFGF_NONE),
-    CFG_STR((char*)"log_path",(char*)"logs",CFGF_NONE),
-    CFG_STR((char*)"server_host",(char*)"127.0.0.1",CFGF_NONE),
-    CFG_INT((char*)"server_port",  7777, CFGF_NONE),
-    CFG_STR((char*)"nickname",(char*)"user",CFGF_NONE),
-    CFG_END()
-};
-
-cfg_t* config::open_cfg()
+void init_config(string filename)
 {
-    cfg_t *cfg = cfg_init(opts, CFGF_NONE);
+    //if (!file_exists(filename.c_str()))
+    //    return false;
 
-    if (cfg_parse(cfg, config_string[CONFIG_CONF_FILENAME].c_str()) == CFG_PARSE_ERROR)
-    {
-        cfg_free(cfg);
-        exit(0);
-    }
+    CFG->add_bool("debug", false);
+    CFG->add_bool("log", false);
+    CFG->add_string("log_path", "logs");
+    CFG->add_string("nickname", "user");
+    CFG->add_string("server_host", "127.0.0.1");
+    CFG->add_string("server_port", "7777");
 
-    return cfg;
+    post_init_config();
 }
 
-config::config()
+void post_init_config()
 {
-    //config_string[CONFIG_CONF_FILENAME] = "pschat-client.conf";
-    load_config();
-}
-
-bool config::load_config()
-{
-    if (!file_exists(config_string[CONFIG_CONF_FILENAME].c_str()))
-        return false;
-
-    cfg_t *cfg = open_cfg();
-
-    config_bool[CONFIG_DEBUG]        = cfg_getbool(cfg, "debug");
-    config_string[CONFIG_LOG_PATH]   = cfg_getstr(cfg, "conf_filename");
-    config_bool[CONFIG_LOG]          = cfg_getbool(cfg, "log_enable");
-    config_string[CONFIG_LOG_PATH]   = cfg_getstr(cfg, "log_path");
-    config_string[CONFIG_LOG_PATH]   = cfg_getstr(cfg, "server_host");
-    config_string[CONFIG_LOG_PATH]   = cfg_getstr(cfg, "server_port");
-
-    cfg_free(cfg);
-
-    return true;
-}
-
-void config::post_init_config()
-{
-    if (!dir_exists(config_string[CONFIG_LOG_PATH]))
-        mkdir(config_string[CONFIG_LOG_PATH].c_str(), 0777);
+    if (!dir_exists(CFG_GET_STRING("log_path")))
+        mkdir(CFG_GET_STRING("log_path").c_str(), 0777);
     else
     {
         stringstream str;
-        str << "rm -f " << config_string[CONFIG_LOG_PATH] << "/*.log";
-        int ret = system(str.str().c_str()); // FIXME check ret
+        str << "rm -f " << CFG_GET_STRING("log_path") << "/*.log";
+        int ret = system(str.str().c_str());
     }
 
     check_config();
     init_log_profiles();
 }
 
-
-void config::init_log_profiles()
+void init_log_profiles()
 {
     log_profile *l_profile;
 
-    if (config_bool[CONFIG_DEBUG])  // deprecated
+    if (CFG_GET_BOOL("debug"))  // deprecated
         LOG_PTR->set_opt(L_DEBUG);
 
-    if (config_bool[CONFIG_LOG]) // deprecated
+    if (CFG_GET_BOOL("log")) // deprecated
         LOG_PTR->set_opt(L_LOG);
     else 
         return;
 
-    if (config_bool[CONFIG_DEBUG])
+    if (CFG_GET_BOOL("debug"))
     {
         l_profile = new log_profile("debug", "");
         l_profile->set_opt(L_VERBOSE | L_DEBUG | L_COLOR);
@@ -86,7 +50,8 @@ void config::init_log_profiles()
     }
 }
 
-int config::load_args(int argc, char **argv)
+
+int load_args(int argc, char **argv)
 {
     int opt, tmp_i;
     opterr = 0;
@@ -99,17 +64,17 @@ int config::load_args(int argc, char **argv)
                 exit(0);
             break;
             case 'd':
-                config_bool[CONFIG_DEBUG] = true;
+                // debug true
             break;
             case 'c':
-                config_string[CONFIG_CONF_FILENAME] = optarg;
+                // new config file = optarg;
                 if (!file_exists(optarg))
                 {
                     cout << "config file " << optarg << " does not exists!" << endl;
                     exit(1);
                 }
-                cout << "* loading config from " << optarg << endl;
-                load_config();
+                cout << "* loading config from " << optarg << endl; //TODO INFO
+                init_config(optarg);
             break;
             case '?':
                 if (optopt == 'c')
@@ -131,7 +96,7 @@ int config::load_args(int argc, char **argv)
     return 0;
 }
 
-void config::help_args()
+void help_args()
 {
     cout << "help: " << endl;
     cout << "-h                  :  help" << endl;
@@ -140,39 +105,7 @@ void config::help_args()
     cout << endl;
 }
 
-void config::check_config() // TODO inserire altri controlli
+void check_config() // TODO inserire i controlli
 {
 
-}
-
-bool config::get_bool(enum config_bool e_conf)
-{
-    if (e_conf >= CONFIG_MAX_BOOL)
-        exit(1); // Errore
-
-    return config_bool[e_conf];
-}
-
-int config::get_int(enum config_int e_conf)
-{
-    if (e_conf >= CONFIG_MAX_INT)
-        exit(1); // Errore
-
-    return config_int[e_conf];
-}
-
-std::string config::get_string(enum config_string e_conf)
-{
-    if (e_conf >= CONFIG_MAX_STRING)
-        exit(1); // Errore
-    
-    return config_string[e_conf];
-}
-
-float config::get_float(enum config_float e_conf)
-{
-    if (e_conf >= CONFIG_MAX_FLOAT)
-        exit(1); // Errore
-
-    return config_float[e_conf];
 }

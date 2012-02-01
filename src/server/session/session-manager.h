815e1a0980196f83f6d1c56e083cdb873fa9f088
lazzalf <lazzalf@localhost>
2012-02-01 18:59:13 +0100

    Session e SessionManager * Ristrutturato sistema per evitare problemi al delete della sessione * Inserite funzioni * Spostati i mutez da UserSession a Session

--- src/server/session/session-manager.h
+++ src/server/session/session-manager.h
@@ -5,8 +5,94 @@
 #include <map>
 #include <pthread.h>
 
-typedef std::map<uint32, UserSession*>  usersession_map;
-typedef std::pair<uint32, UserSession*> usersession_pair;
+class Session
+{
+    public:
+        Session(UserSession* pUser)
+        {
+            m_active = 0;
+            pthread_mutex_init(&mutex_session, NULL);
+            pthread_mutex_init(&mutex_m_active, NULL);
+            pthread_mutex_init(&mutex_net, NULL);
+            pthread_mutex_init(&mutex_exec, NULL);
+        }
+
+        // TODO Inserire tempo di creazione della session per controllo di pacchetti precedenti
+        void SetSession(UserSession* ses) { pUser = ses; m_active = 1;}
+        UserSession* GetUserSession() { return pUser; }
+
+        bool IsActive()
+        {
+            return m_active == 1 ? true : false;
+        }     
+
+        bool IsFree()
+        {
+            return m_active == 0 ? true : false;
+        }
+
+        bool IsToDelete()
+        {
+            return m_active == -1 ? true : false;
+        }
+
+        void ToDelete()
+        {
+            m_active = -1;
+        }
+        
+        void  getlock_session() { pthread_mutex_lock(&mutex_session); }
+        void  releaselock_session() { pthread_mutex_unlock(&mutex_session); }
+        void  getlock_active() { pthread_mutex_lock(&mutex_m_active); }
+        void  releaselock_actives() { pthread_mutex_unlock(&mutex_m_active); }
+
+        // Non bloccante
+        bool getlock_net()
+        {
+            if (pthread_mutex_trylock (&mutex_net) != 0)
+                return  false;
+            else
+                return true;
+        }
+        void  releaselock_net() { pthread_mutex_unlock(&mutex_net); }
+
+        // Non bloccante
+        bool getlock_exec()
+        {
+            if (IsActive() && pUser->RecvSize() == 0)
+                return  false;
+            if (pthread_mutex_trylock (&mutex_exec) != 0)
+                return  false;
+            else
+                return true;
+        }
+        void  releaselock_exec() { pthread_mutex_unlock(&mutex_exec); }
+
+    private:
+        pthread_mutex_t    mutex_session;
+        pthread_mutex_t    mutex_m_active;
+
+        pthread_mutex_t    mutex_net;
+        pthread_mutex_t    mutex_exec;
+
+        int m_active;
+        UserSession* pUser;     
+};
+
+typedef std::map<uint32, Session*>  usersession_map;
+typedef std::pair<uint32, Session*> usersession_pair;
+
+class SessionManagerException : public exception 
+{
+	public:
+		SessionManagerException(const std::string &message, bool inclSysMsg = false) throw();
+		~SessionManagerException() throw();
+
+		const char *what() const throw();
+
+	private:
+		std::string userMessage;  // Exception message
+};
 
 // Classe di gestione delle sessioni aperte
 class SessionManager
@@ -15,11 +101,13 @@ class SessionManager
 	    SessionManager();
         ~SessionManager();
 
-        void addSession (UserSession*);
+        void addSession (UserSession* us);
         void deleteSession (uint32 id);        
         
         UserSession* getNextSessionToServe();
         UserSession* getNextSessionToExecute();
+
+        void SendPacketTo(uint32 id, Packet* new_packet) throw(SessionManagerException);
         
     private:
         usersession_map sessions;

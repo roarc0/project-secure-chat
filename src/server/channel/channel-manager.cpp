607c65898223a580fca6d6ed789d84c0db7d9bf5
lazzalf <lazzalf@localhost>
2012-02-07 16:16:35 +0100

    Channel * Continuati i lavori sui canali, ancora WIP

--- src/server/channel/channel-manager.cpp
+++ src/server/channel/channel-manager.cpp
@@ -20,62 +20,6 @@ const char *ChannelException::what() const throw()
 	return userMessage.c_str();
 }
 
-Channel::Channel(uint32 owner, std::string name, std::string password, uint8 secure, bool persistent)
-{
-    m_name = name;
-    m_owner = owner;
-    m_password = password;
-    m_secure = secure;
-    m_persistent = persistent;
-    gettimeofday(&m_createTime, NULL);
-}
-
-bool Channel::Access(uint32 id, bool& get_admin, std::string password, uint8 secure) throw(ChannelException)
-{
-    if (secure != m_secure)
-        throw ChannelException("Try access a Secure Channel without be logged");
-    if (password != m_password);
-        throw ChannelException("Password is wrong");
-    for (std::list<uint32>::iterator itr = user_list.begin(); itr!= user_list.end(); itr++)
-        if (*itr == id)
-            return false; // Errore Inatteso
-
-    if (user_list.empty())
-    {
-        m_owner = id;
-        get_admin = true;
-    }
-
-    user_list.push_back(id);
-    return true;        
-}
-
-bool Channel::Exit(uint32 id, uint32& new_owner)
-{
-    for (std::list<uint32>::iterator itr = user_list.begin(); itr!= user_list.end(); itr++)
-        if (*itr == id)
-            user_list.erase(itr);
-    
-    if (user_list.empty() && !m_persistent)
-        return false;
-
-    if (m_owner == id && !user_list.empty())
-    {
-        m_owner = user_list.front();
-        new_owner = m_owner;
-        return true;
-    }
-
-    return true;   
-}
-
-uint32 Channel::GetTime() // In millisecondi
-{
-    uint32 seconds  = m_createTime.tv_sec;  // - start.tv_sec;  TODO Quando è stato avviato il programma
-    uint32 useconds = m_createTime.tv_usec; // - start.tv_usec; TODO Quando è stato avviato il programma
-    return ((seconds) * 1000 + useconds/1000.0) + 0.5;
-}
-
 // ChannelManagerException Code
 ChannelManagerException::ChannelManagerException(const string &message, bool inclSysMsg)
   throw() : userMessage(message) 
@@ -96,24 +40,160 @@ const char *ChannelManagerException::what() const throw()
 	return userMessage.c_str();
 }
 
+Channel::Channel(uint32 owner, std::string name, std::string password, uint8 secure, bool persistent)
+{    
+    m_name = name;
+    m_owner = owner;
+    m_password = password;
+    m_secure = secure;
+    m_persistent = persistent;
+
+    user_list.push_back(owner);
+
+    gettimeofday(&m_createTime, NULL);
+    pthread_mutex_init(&mutex_Channel, NULL);
+}
+
+Channel::~Channel()
+{
+    pthread_mutex_destroy(&mutex_Channel);
+}
+
+bool Channel::Access(uint32 id, std::string password, uint8 secure) throw(ChannelException)
+{
+    getlock_Channel();
+    if (secure != m_secure)
+    {
+        releaselock_Channel();
+        throw ChannelException("Try access a Secure Channel without be logged");
+    }
+    if (password != m_password);
+    {
+        releaselock_Channel();
+        throw ChannelException("Password is wrong");
+    }
+    for (std::list<uint32>::iterator itr = user_list.begin(); itr!= user_list.end(); itr++)
+        if (*itr == id)
+        {
+            releaselock_Channel();
+            throw ChannelManagerException("Multiple some id Access to some Channel");
+        }
+
+    if (user_list.empty())
+    {
+        m_owner = id;
+        releaselock_Channel();
+        return true;
+    }
+
+    user_list.push_back(id);
+    releaselock_Channel();
+    return false;        
+}
+
+bool Channel::Exit(uint32 id, uint32& new_owner)
+{
+    getlock_Channel();
+    for (std::list<uint32>::iterator itr = user_list.begin(); itr!= user_list.end(); itr++)
+        if (*itr == id)
+            user_list.erase(itr);
+    
+    if (user_list.empty() && !m_persistent)
+    {
+        releaselock_Channel();
+        return false;
+    }
+
+    if (m_owner == id && !user_list.empty())
+    {
+        m_owner = user_list.front();
+        new_owner = m_owner;
+    }
+    releaselock_Channel();
+    return true;   
+}
+
+uint32 Channel::GetTime() // In millisecondi
+{
+    uint32 seconds  = m_createTime.tv_sec;  // - start.tv_sec;  TODO Quando è stato avviato il programma
+    uint32 useconds = m_createTime.tv_usec; // - start.tv_usec; TODO Quando è stato avviato il programma
+    return ((seconds) * 1000 + useconds/1000.0) + 0.5;
+}
+
 ChannelManager::ChannelManager()
 {
-
+    pthread_mutex_init(&mutex_mapChannel, NULL);
 }
 
 ChannelManager::~ChannelManager()
 {
+    pthread_mutex_destroy(&mutex_mapChannel);
+}
 
+bool ChannelManager::CreateChannel(uint32 id, std::string name, std::string password, uint8 secure, bool persistent) throw(ChannelException)
+{
+    getlock_mapChannel();
+    if (m_mapChannel.find(name) != m_mapChannel.end())
+    {
+        releaselock_mapChannel();
+        throw ChannelException("There is a Channel with some Name");
+    }
+
+    m_mapChannel.insert (std::make_pair(name, new Channel(id, name, password, secure, persistent)));        
+    releaselock_mapChannel();
 }
 
 bool ChannelManager::AccessChannel(uint32 id, std::string name, std::string password, uint8 secure)
 {
+    getlock_mapChannel();
+    mapChannel::iterator itr = m_mapChannel.find(name);
+    if (itr == m_mapChannel.end())
+    {
+        releaselock_mapChannel();
+        throw ChannelException("Channel Name not Found");
+    }
 
+    try
+    {
+        (*itr).second->Access(id, password, secure);
+        releaselock_mapChannel();
+        return true;        
+    }
+    catch (ChannelException exc)
+    {
+        releaselock_mapChannel();
+        throw exc;
+    }
+    catch (ChannelManagerException exc)
+    {
+        releaselock_mapChannel();
+        throw exc;
+    }
+    return false;
 }
 
 uint32 ChannelManager::ExitChannel(uint32 id, std::string name)
 {
-
+    mapChannel::iterator itr = m_mapChannel.find(name);
+    if (itr == m_mapChannel.end())
+    {
+        releaselock_mapChannel();
+        throw ChannelManagerException("Channel Name not Found");
+    }
+    uint32 new_owner = 0;
+    try
+    {
+        if (!(*itr).second->Exit(id, new_owner))
+        {
+            m_mapChannel.erase(itr);
+            releaselock_mapChannel();
+        }
+    }
+    catch (ChannelException exc)
+    {
+        releaselock_mapChannel();
+        throw exc;
+    }
 }
 
 bool ChannelManager::SendPacketToChannel(std::string name, uint32 exclude_id)
--- src/server/channel/channel-manager.h
+++ src/server/channel/channel-manager.h
@@ -7,6 +7,7 @@
 #include <map>
 #include <tr1/unordered_map>
 #include <exception>
+#include <utility>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,33 +25,6 @@ class ChannelException : public exception
 		std::string userMessage;  // Exception message
 };
 
-class Channel
-{
-    public:
-        Channel(uint32 owner, std::string name, std::string password = "", uint8 secure = 0, bool persistent = false);
-        ~Channel();
-        std::list<uint32> user_list;        
-        std::string m_name;        
-
-        void ResetTime() { gettimeofday(&m_createTime, NULL); }        
-        uint32 GetTime(); // In millisecondi
-    
-        bool IsSecure() { return m_secure != 0; }
-        uint32 GetOwner() { return m_owner; }
-
-        bool Access(uint32 id, bool& get_admin, std::string password, uint8 secure) throw(ChannelException);
-        bool Exit(uint32 id, uint32& new_owner);
-    private:
-        timeval m_createTime;
-        std::string m_password;
-        uint8 m_secure;
-        uint32 m_owner;
-        bool m_persistent;
-};
-
-#define UNORDERED_MAP std::tr1::unordered_map
-typedef UNORDERED_MAP<std::string, Channel> mapChannel;
-
 class ChannelManagerException : public exception 
 {
 	public:
@@ -63,16 +37,59 @@ class ChannelManagerException : public exception
 		std::string userMessage;  // Exception message
 };
 
+class Channel
+{
+    public:
+        Channel(uint32 owner, std::string name, std::string password = "", uint8 secure = 0, bool persistent = false);
+        ~Channel();
+        std::list<uint32> user_list;        
+        std::string m_name;    
+
+        void ResetTime() { gettimeofday(&m_createTime, NULL); }        
+        uint32 GetTime(); // In millisecondi
+    
+        bool IsSecure() { return m_secure != 0; }
+        uint32 GetOwner() { return m_owner; }
+
+        bool Access(uint32 id, std::string password, uint8 secure) throw(ChannelException);
+        bool Exit(uint32 id, uint32& new_owner);
+    private:
+        timeval m_createTime;
+        std::string m_password;
+        uint8 m_secure;
+        uint32 m_owner;
+        bool m_persistent;
+
+        pthread_mutex_t    mutex_Channel;
+
+        inline void  getlock_Channel() { pthread_mutex_lock(&mutex_Channel); }
+        inline void  releaselock_Channel() { pthread_mutex_unlock(&mutex_Channel); }
+};
+
+#define UNORDERED_MAP std::tr1::unordered_map
+typedef UNORDERED_MAP<std::string, Channel*> mapChannel;
+
+
 class ChannelManager
 {
 	public:
 		ChannelManager();
 		~ChannelManager();
 
-        bool AccessChannel(uint32 id, std::string name, std::string password, uint8 secure);
+        bool CreateChannel(uint32 id, std::string name, std::string password = "", 
+                           uint8 secure = 0, bool persistent = false) throw(ChannelException);
+        bool AccessChannel(uint32 id, std::string name, std::string password = "", uint8 secure = 0);
         uint32 ExitChannel(uint32 id, std::string name);
 
         bool SendPacketToChannel(std::string name, uint32 exclude_id = 0);
+
+    private:
+        mapChannel m_mapChannel;
+
+        pthread_mutex_t    mutex_mapChannel;
+
+        inline void  getlock_mapChannel() { pthread_mutex_lock(&mutex_mapChannel); }
+        inline void  releaselock_mapChannel() { pthread_mutex_unlock(&mutex_mapChannel); }
 };
 
 #endif

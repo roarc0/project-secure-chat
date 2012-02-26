486da09f8493727a372e731ab0f84b7690edad56
Alessandro Furlanetto <ale.furlanetto@gmail.com>
2012-02-27 00:27:11 +0100

    crypto symmetric funzionante

--- src/shared/cryptography/crypto.cpp
+++ src/shared/cryptography/crypto.cpp
@@ -1,127 +1,62 @@
-#include <stdio.h>
+#include <iostream>
 #include <cstring>
-#include "crypto.h"
 #include <openssl/evp.h>
+#include <openssl/rand.h>
+#include "crypto.h"
+#include <vector>
+
+using namespace std;
 
 void printbyte(char b)
 {
-    char c;
-
-    c = b;
-    c = c >> 4;
-    c = c & 15;
-    printf("%X", c);
-    c = b;
-    c = c & 15;
-    printf("%X:", c);
+    printf("%X%X:", b >> 4 & 15, b & 15);
 }
 
-void select_random_key(char *k, int b)
+void smartBuffer::random()
 {
-    int i;
-    //RAND_bytes(k, b); // qualcuno richiami il mersenne twister!
-    for (i = 0; i < b - 1; i++)
-        printbyte(k[i]);
-    printbyte(k[b-1]);
+    RAND_bytes(val, len); // qualcuno richiami il mersenne twister!
+}
+
+void smartBuffer::print()
+{
+    for (int i = 0; i < len; i++)
+        printbyte(val[i]);
     printf("\n");
 }
 
-EVP_CIPHER_CTX *init_ctx(const unsigned char *key)
+smartBuffer Crypto::sym_encrypt (smartBuffer plaintext)
 {
-    if (!key)
-        return 0;
-
-    EVP_CIPHER_CTX *ctx = (EVP_CIPHER_CTX *) malloc(sizeof(EVP_CIPHER_CTX));
-    EVP_CIPHER_CTX_init(ctx);
-    EVP_EncryptInit(ctx, EVP_des_ecb(), NULL, NULL); // al momento solo ecb 
-
-    printf("key size %d\n", EVP_CIPHER_key_length(EVP_des_ecb()));
-    printf("block size %d\n", EVP_CIPHER_CTX_block_size(ctx));
-
-    EVP_EncryptInit(ctx, NULL, key, NULL);
-
-    return ctx;
+	EVP_CIPHER_CTX* ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));     
+    EVP_CIPHER_CTX_init(ctx);      
+    EVP_EncryptInit(ctx, EVP_des_ecb(), symKey.val, NULL);    
+    int block_size = EVP_CIPHER_CTX_block_size(ctx);
+    
+    Buffer* ciphertext = new Buffer[plaintext.len + block_size + 1];
+    int nc = 0, cursor = 0;
+    EVP_EncryptUpdate(ctx, ciphertext, &nc, plaintext.val, plaintext.len+1);  
+    cursor += nc;
+      
+    EVP_EncryptFinal(ctx, &ciphertext[cursor], &nc);
+    cursor += nc;
+    
+    return smartBuffer(ciphertext, cursor);
 }
 
-unsigned char *
-encrypt(int *ct_len,
-        const unsigned char *plaintext, int pt_len,
-        const unsigned char *key,       int key_size)
+
+smartBuffer Crypto::sym_decrypt (smartBuffer ciphertext)
 {
-    unsigned char *ciphertext;
-    int nc, nctot, i;
-    int ct_ptr, pt_ptr, n;
-
-    if ( !plaintext || !key)
-    {
-        return 0;
-    }
-
-    EVP_CIPHER_CTX *ctx = init_ctx(key);
-
-    n = 10;
-    nc = 0;
-    nctot = 0;
-    ct_ptr = pt_ptr = 0;
-
-    for (i = 0; i < pt_len / n; i++)
-    {
-        EVP_EncryptUpdate(ctx, &ciphertext[ct_ptr], &nc, &plaintext[pt_ptr], n);
-        ct_ptr += nc;
-        pt_ptr += n;
-        nctot += nc;
-    }
-
-    if ( pt_len % n )
-    {
-        EVP_EncryptUpdate(ctx, &ciphertext[ct_ptr], &nc,
-        &plaintext[pt_ptr], pt_len % n);
-        ct_ptr += nc;
-        pt_ptr += pt_len % n;
-        nctot += nc;
-    }
-
-    EVP_EncryptFinal(ctx, &ciphertext[ct_ptr], &nc);
-
-    nctot += nc;
-
-    for (i = 0; i <= *ct_len - 1; i++)
-        printbyte(ciphertext[i]);
-    printf("\n");
-
-    return ciphertext;
+	EVP_CIPHER_CTX* ctx = (EVP_CIPHER_CTX*)malloc(sizeof(EVP_CIPHER_CTX));     
+    EVP_CIPHER_CTX_init(ctx);  
+    EVP_DecryptInit(ctx, EVP_des_ecb(), symKey.val, NULL);
+    int block_size = EVP_CIPHER_CTX_block_size(ctx);
+    Buffer* plaintext = new Buffer[ciphertext.len + block_size + 1];
+    
+    int nc = 0, cursor = 0, ct_len = ciphertext.len;
+    EVP_DecryptUpdate(ctx, &plaintext[cursor], &nc, ciphertext.val, ct_len);
+    cursor += nc;
+    EVP_DecryptFinal(ctx, &plaintext[cursor], &nc);
+    cursor += nc;
+    
+    return smartBuffer(plaintext,cursor);
 }
 
-unsigned char *decrypt(const unsigned char *ciphertext, int ct_len,
-              const unsigned char *key,       int key_size)
-{
-    unsigned char *plaintext;
-    int nc, nctot, i;
-    int ct_ptr, pt_ptr, n;
-
-    if ( !ciphertext || !key)
-    {
-        return 0;
-    }
-
-    EVP_CIPHER_CTX *ctx = init_ctx(key);
-
-    plaintext = (unsigned char *)malloc(ct_len);
-     memset (plaintext, 0, ct_len); // undeclared
-
-    EVP_CIPHER_CTX_init(ctx);
-    EVP_DecryptInit(ctx, EVP_des_ecb(), key, NULL);
-
-    nc = 0;
-    ct_ptr = 0;
-    EVP_DecryptUpdate(ctx, &plaintext[ct_ptr],
-    &nc, &ciphertext[ct_ptr], ct_len);
-    EVP_DecryptFinal(ctx, &plaintext[ct_ptr],
-    &nc);
-
-    for (i = 0; i <= ct_len - 1; i++)
-        printf("%c:", plaintext[i]);
-    printf("%s\n", plaintext);
-
-    return plaintext;
-}
--- src/shared/cryptography/crypto.h
+++ src/shared/cryptography/crypto.h
@@ -1,12 +1,46 @@
-#include <openssl/evp.h>
-
 #ifndef CRYPT_H
 #define CRYPT_H
+#include <openssl/evp.h>
+#include <string>
 
+typedef unsigned char Buffer;
+using namespace std;
+//{}
 void printbyte(char);
-void select_random_key(char*, int);
 
-char *encrypt(int*, const char*, int, const char *, int);
-char *decrypt(const char *, int, const char *, int);
+struct smartBuffer { // not so smart
+    Buffer *const val;
+    int len;
+    smartBuffer(int l) : val(new Buffer[l]), len(l) { random(); };
+    smartBuffer(char* v) : val((Buffer*)v), len(strlen(v)){};
+    smartBuffer(string v) : val((Buffer*)v.c_str()), len(v.size()){};
+    smartBuffer(Buffer* v = 0, int l = 0) : val(v),len(l){};
+    
+    void print();
+    void random();
+};
+
+class CryptoException{};
+
+class Crypto
+{
+    smartBuffer symKey, asymKey;
+    
+    public:
+    Crypto (smartBuffer k = smartBuffer()): symKey(k){};
+    
+    smartBuffer sym_encrypt(smartBuffer);
+    smartBuffer sym_decrypt(smartBuffer);
+    smartBuffer asym_encrypt(smartBuffer);
+    smartBuffer asym_decrypt(smartBuffer);
+};
+
+ostream& operator<<(ostream& o, smartBuffer s)
+{
+   o<<s.val;
+}
 
 #endif
+
+
+

cada6023df7953cdc5378ca4e74b501dd6a9e6b6
lazzalf <lazzalf@localhost>
2012-03-09 16:58:06 +0100

    Channel * Pulita Classe Channel per nuova implementazione ChannelManager * Aggiornate eccezioni * Inserite nuove funzioni

--- src/server/channel/channel.h
+++ src/server/channel/channel.h
@@ -3,49 +3,11 @@
 
 #include "../../common.h"
 
-#include <list>
-#include <map>
-#include <tr1/unordered_map>
-#include <exception>
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-
-#define UNORDERED_MAP std::tr1::unordered_map
-typedef UNORDERED_MAP<std::string, Channel>
-
-class ChannelException : public Exception;
-
-class Channel
-{
-    public:
-        Channel(uint32 owner, std::string name, std::string password = "", uint8 secure = 0, bool persistent = false);
-        ~Channel();
-        std::list<uint32> user_list;        
-        std::string m_name;        
-
-        void ResetTime() { gettimeofday(&m_createTime, NULL); }        
-        uint32 GetTime(); // In millisecondi
-    
-        bool IsSecure() { return m_secure != 0; }
-        uint32 GetOwner() { return m_owner; }
-
-        bool Access(uint32 id, bool& get_admin, std::string password, uint8 secure) throw(ChannelException);
-        bool Exit(uint32 id, uint32& new_owner);
-    private:
-        timeval m_createTime;
-        std::string m_password;
-        uint8 m_secure;
-        uint32 m_owner;
-        bool m_persistent;
-}
-
-class ChannelManagerException : public exception 
+class ChannelException : public exception 
 {
 	public:
-		ChannelManagerException(const std::string &message, bool inclSysMsg = false) throw();
-		~ChannelManagerException() throw();
+		ChannelException(const std::string &message, bool inclSysMsg = false) throw();
+		~ChannelException() throw();
 
 		const char *what() const throw();
 
@@ -53,17 +15,12 @@ class ChannelManagerException : public exception
 		std::string userMessage;  // Exception message
 };
 
-class ChannelManager
+class Channel
 {
-	public:
-		ChannelManager();
-		~ChannelManager();
-
-        bool AccessChannel(uint32 id, std::string name, std::string password, uint8 secure)
-        uint32 ExitChannel(uint32 id, std::string name);
-
-	private:
-        ChannelManager()
+    public:
+        Channel(std::string& c_name);
+        ~Channel();
+    private:
 };
 
 #endif

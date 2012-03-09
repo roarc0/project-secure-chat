385d3e51dbd5ca0b6cba262466a34470214ae48c
lazzalf <lazzalf@localhost>
2012-03-09 16:22:50 +0100

    Apocalisse * Inserito ChannelUpdate, nuovo ChannelManager (WIP), MethodRequest

--- src/shared/typedefs.h
+++ src/shared/typedefs.h
@@ -3,6 +3,16 @@
 #ifndef TYPEDEFS_H
 #define TYPEDEFS_H
 
+#if COMPILER == COMPILER_INTEL
+#include <ext/hash_map>
+#elif COMPILER == COMPILER_GNU && (__GNUC__ > 4 || __GNUC__ == 4 && __GNUC_MINOR__ >= 3)
+#include <tr1/unordered_map>
+#elif COMPILER == COMPILER_GNU && __GNUC__ >= 3
+#include <ext/hash_map>
+#endif
+
+#define UNORDERED_MAP std::tr1::unordered_map
+
 typedef unsigned char uint8;
 typedef unsigned short int uint16;
 typedef unsigned long int uint32;

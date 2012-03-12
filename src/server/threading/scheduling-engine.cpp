78772cfdb6d8a4525b8c92a60025239b14c68c60
lazzalf <lazzalf@localhost>
2012-03-12 17:42:27 +0100

    Inserita classe semaphore.h e inserita dentro lo scheduling-engine

--- src/server/threading/scheduling-engine.cpp
+++ src/server/threading/scheduling-engine.cpp
@@ -1,6 +1,6 @@
 SchedulingEngine* SchedulingEngine::_instance = NULL;
 
-SchedulingEngine::SchedulingEngine() : b_active(false)
+SchedulingEngine::SchedulingEngine() : sem(m_mutex), b_active(false)
 {
 
 }
@@ -12,7 +12,8 @@ SchedulingEngine::~SchedulingEngine()
 
 void SchedulingEngine::Initialize(uint32 n_thread)
 {
-
+    // Creazione N thread e passaggio di variabili
+    b_active = true;
 }
 
 int SchedulingEngine::Deactivate()
@@ -26,6 +27,16 @@ bool SchedulingEngine::IsActive()
 }
 
 int SchedulingEngine::Execute(MethodRequest* m_req)
-{
+{   
+    q_method.add(m_req);
+    sem.Signal();
     return 0;
+}
+
+MethodRequest* SchedulingEngine::GetNextMethod()
+{
+    sem.Wait();
+    MethodRequest* ret = NULL;
+    q_method.next(ret);
+    return ret;
 }

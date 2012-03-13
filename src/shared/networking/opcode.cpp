c861239894929514c5db2ecfd48ddf4fb4f175c4
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2012-03-13 14:53:48 +0100

    * WIP rivoluzioni generali

--- src/shared/networking/opcode.cpp
+++ src/shared/networking/opcode.cpp
@@ -1,3 +1,5 @@
+#include "opcode.h"
+
 OpcodeHandler opcodeTable[NUM_MSG_TYPES] =
 {
     /*0x000*/ { "MSG_NULL_ACTION", STATUS_NEVER, PROCESS_INPLACE, &Session::Handle_NULL },
--- src/shared/networking/socket-base.h
+++ src/shared/networking/socket-base.h
@@ -1,31 +1,72 @@
-#ifndef _SOCKET_BASE_H
-#define _SOCKET_BASE_H
+/*
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <fcntl.h>
+#ifndef SOCKET_BASE_H
+#define SOCKET_BASE_H
 
-#include <unistd.h>
-#include <errno.h>
-#include <string.h>
+#include <sys/types.h>       // For data types
+#include <sys/socket.h>      // For socket(), connect(), send(), and recv()
+#include <netdb.h>           // For gethostbyname()
+#include <arpa/inet.h>       // For inet_addr()
+#include <unistd.h>          // For close()
+#include <netinet/in.h>      // For sockaddr_in
+#include <string>
 
-#include "../exception.h"
+#include "../common.h"
+#include "socket-exception.h"
 
-class SocketServerException : public Exception;
+using namespace std;
+
+#define MAX_QUEUE_CONNECTIONS 16
+#define INVALID_SOCKET 0
 
 class SocketBase
 {
-  public:
-    SocketBase();
-    SocketBase(int sock);
-    ~SocketBase();
+    public:
+        ~SocketBase();
+        SocketBase(int type, int protocol) throw(SocketException);
 
-  private:
-    int sock;
+        string           getLocalAddress()
+                         throw(SocketException);
+        unsigned short   getLocalPort()
+                         throw(SocketException);
 
+        void             setLocalPort(unsigned short localPort)
+                         throw(SocketException);
+        void             setLocalAddressAndPort(const string &localAddress, 
+                                                unsigned short localPort = 0)
+                         throw(SocketException);
+        string           getForeignAddress()
+                         throw(SocketException);
+        unsigned short   getForeignPort()
+                         throw(SocketException);
+
+        static unsigned short resolveService(const string &service,
+                                             const string &protocol = "tcp");
+
+        void             initSocket()
+                         throw(SocketException);
+
+        void             send(const void *buffer, int bufferLen)
+                         throw(SocketException);
+        int              recv(void *buffer, int bufferLen)
+                         throw(SocketException);
+
+    private:
+        int sockDesc;
+        int domain, type, protocol;
 };
 
 #endif

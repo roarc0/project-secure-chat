9945ad5ef96977caaa49314924bb2910138283c9
lazzalf <lazzalf@localhost>
2012-03-19 14:56:20 +0100

    Inseriti alcuni esempi di opcode

--- src/server/networking/opcode.h
+++ src/server/networking/opcode.h
@@ -8,14 +8,32 @@ enum MessageTypes
     MSG_NULL_ACTION                                 = 0x000,
     CMSG_MESSAGE                                    = 0x001,
     SMSG_MESSAGE                                    = 0x002,
-    NUM_MSG_TYPES                                   = 0x003,
+    CMSG_JOIN_CHANNEL                               = 0x003,
+    CMSG_LEAVE_CHANNEL                              = 0x004,
+    SMSG_CHANNEL_NOTIFY                             = 0x005,
+    CMSG_CHANNEL_LIST                               = 0x006,
+    SMSG_CHANNEL_LIST                               = 0x007,
+    CMSG_CHANNEL_PASSWORD                           = 0x008,
+    CMSG_CHANNEL_SET_OWNER                          = 0x009,
+    CMSG_CHANNEL_OWNER                              = 0x00A,
+    CMSG_CHANNEL_MODERATOR                          = 0x00B,
+    CMSG_CHANNEL_UNMODERATOR                        = 0x00C,
+    CMSG_CHANNEL_MUTE                               = 0x00D,
+    CMSG_CHANNEL_UNMUTE                             = 0x00E,
+    CMSG_CHANNEL_INVITE                             = 0x00F,
+    CMSG_CHANNEL_KICK                               = 0x010,
+    CMSG_CHANNEL_BAN                                = 0x011,
+    CMSG_CHANNEL_UNBAN                              = 0x012,
+    CMSG_CHANNEL_ANNOUNCEMENTS                      = 0x013,
+    CMSG_CHANNEL_MODERATE                           = 0x014,
+    NUM_MSG_TYPES                                   = 0x015,
 };
 
 enum SessionStatus
 {
     STATUS_LOGGING = 0,                         // Collegato               
     STATUS_LOGGED,                              // Loggato           
-    STATUS_TRANSFER,                            // Trasferimento di Cell       
+    STATUS_TRANSFER,                            // Trasferimento      
     STATUS_LOGOUT,                              // Sta per sloggarsi
     STATUS_NEVER,                               // Non accettato 
     STATUS_UNHANDLED                            // Non valido
@@ -23,9 +41,9 @@ enum SessionStatus
 
 enum PacketProcessing
 {
-    PROCESS_INPLACE = 0,                         // processiamo il pacchetto dove si riceve
-    PROCESS_THREADUNSAFE,                        // il pacchetto non è thread-safe
-    PROCESS_THREADSAFE                           // il pacchetto è thread-safe
+    PROCESS_INPLACE = 0,                         // il pacchetto è gestito in single o in multithread (a seconda del caso)
+    PROCESS_THREADUNSAFE,                        // il pacchetto è gestito solo in singletread
+    PROCESS_THREADSAFE                           // il pacchetto è gestito solo in multithread
 };
 
 struct OpcodeHandler

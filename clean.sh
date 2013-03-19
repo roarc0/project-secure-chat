aa05b0ec48fcc137f7b6e4f8c10f9178daa97933
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2013-03-19 13:13:47 +0100

    * removing autotools build system * cleanups

--- clean.sh
+++ clean.sh
@@ -30,7 +30,7 @@ find -name *.o -exec rm -f {} \; > /dev/null 2>&1
 find -name .dirstamp -exec rm -f {} \; > /dev/null 2>&1
 find -name .deps -exec rm -rf {} \; > /dev/null 2>&1
 
-rm -rf autom4te.cache/ stamp-h1 config.status install-sh \
-        config.log aclocal.m4 Makefile Makefile.in configure config.status config.h \
-       config.h.in depcomp missing  INSTALL *.log *.gdb compile \ > /dev/null 2>&1
+#rm -rf autom4te.cache/ stamp-h1 config.status install-sh \
+#        config.log aclocal.m4 Makefile Makefile.in configure config.status config.h \
+#       config.h.in depcomp missing  INSTALL *.log *.gdb compile \ > /dev/null 2>&1
 

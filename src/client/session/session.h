6876beb723b3b9bc0e2bb16dd382b49f52e7828f
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2013-03-22 15:51:35 +0100

    Adding client session Cmake fixes

---
+++ src/client/session/session.h
@@ -0,0 +1,15 @@
+#ifndef _SESSION_H
+#define _SESSION_H
+
+#include "session/sessionbase.h"
+
+class Session : public SessionBase
+{
+    public:
+        Session();
+        ~Session();
+
+    private:
+};
+
+#endif

e968d30b4a819181d741044cce26944155f5072a
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2013-04-18 10:34:02 +0200

    * Crypto class updated to use AES128/256 symmetric encryption * Crypto class now uses bytebuffer (da spostare in utility)

---
+++ src/shared/crypto/crypto.h
@@ -0,0 +1,50 @@
+#ifndef _CRYPTO_H_
+#define _CRYPTO_H_
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <cmath>
+#include <cstdlib>
+#include <openssl/aes.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+
+#include "../networking/bytebuffer.h"
+
+const int BlockSize=16;
+
+using namespace std;
+
+void printbyte(char);
+
+class CryptoException{};
+
+class Crypto
+{
+    ByteBuffer symKey;
+    
+    public:
+    Crypto (ByteBuffer k): symKey(k){};
+
+    void setSymKey(ByteBuffer);
+    ByteBuffer getSymKey();
+    
+    int AesEncrypt(const ByteBuffer &key,
+               const std::string &plaintext,
+               ByteBuffer &ciphertext);
+    
+    int AesDecrypt(const ByteBuffer &key,
+               const ByteBuffer &ciphertext,
+               std::string &plaintext);
+    
+    //ByteBuffer SymEncrypt(const std::string);
+    //smartBuffer SymDecrypt(const ByteBuffer);
+    //smartBuffer AsymEncrypt(smartBuffer);
+    //smartBuffer AsymDecrypt(smartBuffer);
+};
+
+#endif
+
+
+

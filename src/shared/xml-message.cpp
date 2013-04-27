ad03b932209afcc75506bb1f909501127470540c
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2013-04-27 15:28:19 +0200

    * XML: functions that generates messages used to update user status join/leave

--- src/shared/xml-message.cpp
+++ src/shared/xml-message.cpp
@@ -25,11 +25,12 @@ std::string XMLBuildMessage(const char* name, const char* content)
     stringstream ss;
     ss << doc;
  
-    return ss.str(); 
+    std::string sss = XMLBuildUpdate("alec", "join");
+    std::string nx,sx;
+    XMLReadUpdate(sss.c_str(), nx,sx);
+    INFO("debug", "XML: %s - %s #\n", nx.c_str(),sx.c_str());
  
-    //elem->SetAttribute( "priority", 2 );
-    //elem = elem->NextSiblingElement();
-    //doc.SaveFile(filename);
+    return ss.str(); 
 }
 
 void XMLReadMessage(const char *str, string& name, string& content)
@@ -83,6 +84,79 @@ void XMLReadMessage(const char *str, string& name, string& content)
             else if (elemName == "content")
             {
                 content = DecodeBase64(text->Value());
+            }
+        }
+     }
+}
+
+std::string XMLBuildUpdate(const char* name, const char* status)
+{
+    TiXmlDocument doc;
+    TiXmlDeclaration * decl = new TiXmlDeclaration( "1.0", "", "" );
+    TiXmlElement * element = new TiXmlElement( "update" );
+    
+    TiXmlElement * element_name = new TiXmlElement( "name" );
+    TiXmlText * text_name = new TiXmlText( EncodeBase64(name) );
+
+    element->LinkEndChild( element_name );
+    element_name->LinkEndChild( text_name );
+    
+    element_name->SetAttribute("status", EncodeBase64(status));
+    
+    doc.LinkEndChild( decl );
+    doc.LinkEndChild( element );
+
+    stringstream ss;
+    ss << doc;
+
+    INFO("debug","XML: %s\n", ss.str().c_str());
+     
+    return ss.str(); 
+}
+
+void XMLReadUpdate(const char *str, string& name, string& status)
+{
+    if(!str)
+        return;
+        
+    TiXmlDocument doc;
+    doc.Parse(str);
+    
+    TiXmlElement* root = doc.FirstChildElement();
+
+    if(root == NULL)
+    {
+        cerr << "Failed to load file: No root element." << endl;
+        doc.Clear();
+        return;
+    }
+    
+    for(TiXmlElement* elem = root->FirstChildElement();
+        elem != NULL;
+        elem = elem->NextSiblingElement())
+    {
+        const char* attr;
+        string elemName = elem->Value();
+        if(elemName == "name")
+        {
+            attr = elem->Attribute("status");
+            if(attr != NULL)
+            {
+                status = DecodeBase64(attr);
+            }
+        }
+        
+        for(TiXmlNode* e = elem->FirstChild();
+            e != NULL;
+            e = e->NextSibling())
+        {
+            TiXmlText* text = e->ToText();
+            if(text == NULL)
+                continue;
+
+            if(elemName == "name")
+            {
+                name = DecodeBase64(text->Value());
             }
         }
      }
--- src/shared/xml-message.h
+++ src/shared/xml-message.h
@@ -4,10 +4,13 @@
 #include <tinyxml.h>
 #include <sstream>
 #include <string>
+#include "common.h"
 
 using namespace std;
 
 string XMLBuildMessage(const char *, const char *);
 void   XMLReadMessage(const char *, string&, string&);
+std::string XMLBuildUpdate(const char *, const char*);
+void XMLReadUpdate(const char *str, string& name, string& status);
 
 #endif
